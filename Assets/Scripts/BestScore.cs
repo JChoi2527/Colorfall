3f0aa273697d74823808ff74043d91fcec6d8379
JakeC0527 <jakec0527@gmail.com>
2017-06-30 19:59:04 -0700

    UI changes

---
+++ Assets/Scripts/BestScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BestScore : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    //GameObject myTextgameObject; // gameObject in Hierarchy
+    public Text scoreText;
+
+    void Start()
+    {
+        //GetComponent<Text>() = Values.score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = "Best: " + PlayerPrefs.GetInt("High Score");
+    }
+}
